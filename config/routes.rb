e59e33dcbb1167fdf81b7db4404654a797a5cde6
Arpan <arpansac@gmail.com>
2017-09-17 14:37:52 +0530

    added form for comments and also list of comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :posts, only: [:index, :create, :destroy]
+  resources :comments, only: [:create, :destroy]
 
   root "posts#index"
 end
